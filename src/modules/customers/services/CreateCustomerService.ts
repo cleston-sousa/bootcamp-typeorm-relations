253147eb6a0e90c78e7d65a5ad0a1db47aef1412
Cleston Sousa <cleston.vs@gmail.com>
2020-12-21 02:43:54 -0300

    solved challenge

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,20 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) { }
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const existingCustomer = await this.customersRepository.findByEmail(email);
+
+    if (existingCustomer) {
+      throw new AppError('E-mail already registered');
+    }
+
+    const customer = await this.customersRepository.create({ name, email });
+    return customer;
   }
 }
 
--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -4,8 +4,12 @@ import AppError from '@shared/errors/AppError';
 
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
+
+import IUpdateProductsQuantityDTO from '@modules/products/dtos/IUpdateProductsQuantityDTO';
 import Order from '../infra/typeorm/entities/Order';
 import IOrdersRepository from '../repositories/IOrdersRepository';
+
+import ICreateOrderDTO from '../dtos/ICreateOrderDTO';
 
 interface IProduct {
   id: string;
@@ -20,13 +24,66 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
-    private ordersRepository: IOrdersRepository,
+    @inject('OrdersRepository') private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
-  ) {}
+  ) { }
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+    if (!customer) {
+      throw new AppError('Invalid Customer');
+    }
+
+    const persistedProducts = await this.productsRepository.findAllById(
+      products,
+    );
+
+    if (persistedProducts.length !== products.length) {
+      throw new AppError('Invalid Products quantity');
+    }
+
+    const updateProducts: IUpdateProductsQuantityDTO[] = [];
+
+    const productsToInsert = products.map(product => {
+      const persistedProduct = persistedProducts.find(
+        item => item.id === product.id,
+      );
+
+      if (!persistedProduct) {
+        throw new AppError('Invalid Product selected');
+      }
+
+      if (persistedProduct.quantity < product.quantity) {
+        throw new AppError(
+          `Invalid Product quantity selected for: ${persistedProduct.name}`,
+        );
+      }
+
+      updateProducts.push({
+        id: product.id,
+        quantity: persistedProduct.quantity - product.quantity,
+      });
+
+      return {
+        product_id: product.id,
+        quantity: product.quantity,
+        price: persistedProduct.price,
+      };
+    });
+
+    const data: ICreateOrderDTO = {
+      customer,
+      products: productsToInsert,
+    };
+
+    const order = await this.ordersRepository.create(data);
+
+    await this.productsRepository.updateQuantity(updateProducts);
+
+    return order;
   }
 }
 
